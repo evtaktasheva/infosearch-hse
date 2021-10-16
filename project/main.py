34b4b4a01b568663a0aff8f1edc3dee05c653f1a
tak_ty <42929099+evtaktasheva@users.noreply.github.com>
2021-10-17 01:10:49 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,11 +1,9 @@
 from classes import *
 import streamlit as st
 from datetime import datetime as time
-import os
 
 
 def main():
-    st.write(os.listdir(os.path.join(os.getcwd(), 'project/corpus')))
     st.markdown("<h1 style='text-align: center; font-size: 700%'>✨🔮✨</h1>",
                 unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center;'>❤️ Я могу ответить на твои "
