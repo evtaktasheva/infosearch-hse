f7c86f735409d57bca66abc2b173f20296ab1de4
tak_ty <42929099+evtaktasheva@users.noreply.github.com>
2021-10-18 17:21:49 +0300

    Update utilities.py

--- project/utilities.py
+++ project/utilities.py
@@ -1,6 +1,8 @@
 import pymorphy2
 from nltk.corpus import stopwords
 from nltk.tokenize import wordpunct_tokenize
+import nltk
+nltk.download('stopwords')
 
 
 def lemmatize(text: str, model: pymorphy2.analyzer.MorphAnalyzer) -> str:
